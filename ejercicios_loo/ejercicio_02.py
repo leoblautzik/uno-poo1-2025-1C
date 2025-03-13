16549d3fa3063dde69cd170ad8e65422eb6bd561
leoblautzik <leoblautzik@gmail.com>
2025-03-13 09:05:22 -0300

    Primer commit 2025-1C, ejercicios de repaso

---
+++ ejercicios_loo/ejercicio_02.py
@@ -0,0 +1,17 @@
+"""ejercicio 2"""
+
+
+def main():
+    """Leer dos valores enteros e informar la suma y su cociente."""
+    a = int(input("Ingrese el primer numero: "))
+    b = int(input("ingrese el segundo numero: "))
+
+    print("La suma de los numeros ingresados es:", a + b)
+    print("La resta de los numeros ingresados es:", a - b)
+    print("El producto de los numeros ingresados es:", a * b)
+    print("La division entera de los numeros ingresados es:", a // b)
+    print(f"El cociente de los numeros ingresados es: {a / b:,.2f}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ ejercicios_loo/nombre_edad.py
@@ -0,0 +1,9 @@
+def main():
+    nombre = input("Ingresa tu nombre: ")
+    edad = input("Ingresa tu edad: ")
+
+    print(f"Tu nombre es {nombre} y tienes {edad} años")
+
+
+if __name__ == "__main__":
+    main()
