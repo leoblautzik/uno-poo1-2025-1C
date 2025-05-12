90a5f1c6dd3982536d6a3f404297250e249702d4
leoblautzik <leoblautzik@gmail.com>
2025-05-12 11:54:12 -0300

    Collecciones 1

--- workspace-2025-1c/Herencia/src/juegoDeEstrategia/Soldado.java
+++ workspace-2025-1c/Herencia/src/juegoDeEstrategia/Soldado.java
@@ -1,19 +1,20 @@
 package juegoDeEstrategia;
 
-public class Soldado extends Unidad implements Bebedor{
+public class Soldado extends Unidad implements Bebedor {
+
 	private int energia = 100;
 
 	public Soldado(int posicion) {
 		super(200, 10, posicion);
 	}
-	
+
 	public int getEnergia() {
 		return this.energia;
 	}
 
 	@Override
 	public void atacar(Unidad enemigo) {
-		
+
 		if (this.puedeAtacar(enemigo)) {
 			this.energia -= 10;
 			enemigo.recibirDanio(10);
@@ -21,7 +22,7 @@ public class Soldado extends Unidad implements Bebedor{
 	}
 
 	public boolean puedeAtacar(Unidad enemigo) {
-		if(enemigo.estaMuerto()) {
+		if (enemigo.estaMuerto()) {
 			throw new Error("No podes atacar a un muerto");
 		}
 
@@ -31,7 +32,6 @@ public class Soldado extends Unidad implements Bebedor{
 	@Override
 	public void beberAgua() {
 		this.energia = 100;
-		
 	}
 
 }
