7b2b6d4dce470adf818629bd2b9fddbfebe82a93
leoblautzik <leoblautzik@gmail.com>
2025-05-19 11:09:49 -0300

    Collecciones 3

--- colecciones/expresion_balanceada.py
+++ colecciones/expresion_balanceada.py
@@ -1,5 +1,35 @@
+from collections import deque
+
+
+class ExpresionBalanceada:
+    def es_de_abrir(self, c) -> bool:
+        return c in "{[("
+
+    def es_de_cerrar(self, c) -> bool:
+        return c in "}])"
+
+    def son_parejita(self, leido, desapilado) -> bool:
+        dicc = {"}": "{", "]": "[", ")": "("}
+        return desapilado == dicc[leido]
+
+    def esta_balanceada(self, expresion) -> bool:
+        pila = deque()
+        for c in expresion:
+            if self.es_de_abrir(c):
+                pila.append(c)
+
+            elif self.es_de_cerrar(c):
+                if len(pila) == 0 or not self.son_parejita(c, pila.pop()):
+                    return False
+            else:
+                return False
+
+        return len(pila) == 0
+
+
 def main():
-    pass
+    eb = ExpresionBalanceada()
+    print(eb.esta_balanceada("()"))
 
 
 if __name__ == "__main__":
--- colecciones/expresion_balanceada_test.py
+++ colecciones/expresion_balanceada_test.py
@@ -1,9 +1,35 @@
 import unittest
 
+from expresion_balanceada import ExpresionBalanceada
+
 
 class TestColeccionesExpresionBalanceada(unittest.TestCase):
     def test_example(self):
         self.assertEqual(1 + 1, 2)
+
+    def test_balanceada_enunciado(self):
+        eb = ExpresionBalanceada()
+        self.assertTrue(eb.esta_balanceada("[()]{}{[()()]()}"))
+
+    def test_desbalanceada_enunciado(self):
+        eb = ExpresionBalanceada()
+        self.assertFalse(eb.esta_balanceada("[(])"))
+
+    def test_todos_de_abrir(self):
+        eb = ExpresionBalanceada()
+        self.assertFalse(eb.esta_balanceada("[{({{{{((((("))
+
+    def test_todos_de_cerrar(self):
+        eb = ExpresionBalanceada()
+        self.assertFalse(eb.esta_balanceada("))))))))]]]]]]]]]}}}}}}}}"))
+
+    def test_de_cerrar_de_abrir(self):
+        eb = ExpresionBalanceada()
+        self.assertFalse(eb.esta_balanceada(")("))
+
+    def test_cualquier_cadena(self):
+        eb = ExpresionBalanceada()
+        self.assertFalse(eb.esta_balanceada("abc"))
 
 
 if __name__ == "__main__":
